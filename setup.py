0d4ef1ec744384b0f58f38cf2d2851c55e660a2a
castix <vincenzo.castiglia@axant.it>
2022-05-15 18:49:05 +0200

    move tests to separate directory, add classifiers, add extra test dependencies

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 from setuptools import setup
 
 
-with open('README.md') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='i3-workspace-names-daemon',
-      version='0.15.0',
-      description='Dynamically update the name of each i3wm workspace using font-awesome icons or the names of applications running in each workspace.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/cboddy/i3-workspace-names-daemon',
-      license='MIT',
-      zip_safe=False,
-      py_modules=['i3_workspace_names_daemon', 'fa_icons'],
-      install_requires=["i3ipc"],
-      author='Chris Boddy',
-      author_email='chris@boddy.im',
-      entry_points={
-          'console_scripts': [
-              'i3-workspace-names-daemon=i3_workspace_names_daemon:main'
-          ]
-      }
-      )
+
+setup(
+    name='i3-workspace-names-daemon',
+    version='0.15.0',
+    description='Dynamically update the name of each i3wm workspace using font-awesome icons or the names of applications running in each workspace.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cboddy/i3-workspace-names-daemon',
+    license='MIT',
+    zip_safe=False,
+    py_modules=['i3_workspace_names_daemon', 'fa_icons'],
+    install_requires=["i3ipc"],
+    extras_require={'testing': ['pytest', 'pytest-cov', 'pytest-randomly']},
+    author='Chris Boddy',
+    author_email='chris@boddy.im',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'i3-workspace-names-daemon=i3_workspace_names_daemon:main'
+        ]
+    },
+)

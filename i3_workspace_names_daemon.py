d4913adaeae6ba66d5b6716cd57bd4de1b152c5d
Castix <castix@autistici.org>
2024-10-02 23:00:45 +0200

    codecov config

--- i3_workspace_names_daemon.py
+++ i3_workspace_names_daemon.py
@@ -9,10 +9,6 @@ import i3ipc
 from sys import stderr, argv
 from fa_icons import icons as fa_icons
 
-def _false():
-    ...
-    # just a test
-
 I3_CONFIG_PATHS = tuple(
     os.path.expanduser(path)
     for path in (

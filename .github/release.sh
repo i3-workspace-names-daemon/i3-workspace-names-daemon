591d1d24c0596f6679cff926b5099e47815488a3
Castix <castix@autistici.org>
2024-11-01 22:46:22 +0100

    Close issue #18
    Realeases are initially draft.
    
    Reasons are explained in the discussion
    
    Mantainers need to use a web browser and a github account to make github
    releases (at least one of the mantainers should)

--- .github/release.sh
+++ .github/release.sh
@@ -48,13 +48,3 @@ curl -L \
 -H "Content-Type: application/octet-stream" \
 "https://uploads.github.com/repos/$I3WND/$I3WND/releases/${RELEASE_ID}/assets?name=${newest_deb}.md5" \
 --data-binary "@${newest_deb}.md5" 2>&1 >/dev/null
-
-# a completely uploaded release is releasable
-curl -L \
--X PATCH \
--H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer ${GH_TOKEN}" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/$I3WND/$I3WND/releases/RELEASE_ID \
--d '{"draft":false}' 2>&1 >/dev/null
-
